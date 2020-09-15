b076b9f1e5ba3ccc81fd4b1863392b8cca1a6784
csidi90 <csidiropoulos1990@gmail.com>
2020-09-15 14:43:40 +0200

    first commit

--- talib.js
+++ talib.js
@@ -15,7 +15,23 @@ const belowMA = function (closePrices, period) {
   return lastPrice < value;
 };
 
+const aboveEMA = function (closePrices, period) {
+  let result = talib.EMA(closePrices, period, talib.MATypes.EMA);
+  let value = _.last(result);
+  let lastPrice = _.last(closePrices);
+  return lastPrice > value;
+};
+
+const belowEMA = function (closePrices, period) {
+  let result = talib.EMA(closePrices, period, talib.MATypes.EMA);
+  let value = _.last(result);
+  let lastPrice = _.last(closePrices);
+  return lastPrice < value;
+};
+
 module.exports = {
   aboveMA,
+  aboveEMA,
   belowMA,
+  belowEMA,
 };
