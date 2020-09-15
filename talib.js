8bff99e4fd1814234d687a6258bfa84345888f53
csidi90 <csidiropoulos1990@gmail.com>
2020-09-15 14:57:39 +0200

    first commit

--- talib.js
+++ talib.js
@@ -29,9 +29,55 @@ const belowEMA = function (closePrices, period) {
   return lastPrice < value;
 };
 
+const bullishSMACross = function (closePrices, shortPeriod, longPeriod) {
+  let result_short = talib.MA(closePrices, shortPeriod);
+  let result_long = talib.MA(closePrices, longPeriod);
+  let short_value = _.last(result_short);
+  let prev_short_value = result_short[result_short.length - 2];
+  let long_value = _.last(result_long);
+  let prev_long_value = result_long[result_long.length - 2];
+
+  return shortValue > longValue && prev_short_value <= prev_long_value;
+};
+const bearishSMACross = function (closePrices, shortPeriod, longPeriod) {
+  let result_short = talib.MA(closePrices, shortPeriod);
+  let result_long = talib.MA(closePrices, longPeriod);
+  let short_value = _.last(result_short);
+  let prev_short_value = result_short[result_short.length - 2];
+  let long_value = _.last(result_long);
+  let prev_long_value = result_long[result_long.length - 2];
+
+  return shortValue < longValue && prev_short_value >= prev_long_value;
+};
+
+const bullishEMACross = function (closePrices, shortPeriod, longPeriod) {
+  let result_short = talib.EMA(closePrices, shortPeriod, talib.MATypes.EMA);
+  let result_long = talib.EMA(closePrices, longPeriod, talib.MATypes.EMA);
+  let short_value = _.last(result_short);
+  let prev_short_value = result_short[result_short.length - 2];
+  let long_value = _.last(result_long);
+  let prev_long_value = result_long[result_long.length - 2];
+
+  return shortValue > longValue && prev_short_value <= prev_long_value;
+};
+const bearishEMACross = function (closePrices, shortPeriod, longPeriod) {
+  let result_short = talib.EMA(closePrices, shortPeriod, talib.MATypes.EMA);
+  let result_long = talib.EMA(closePrices, longPeriod, talib.MATypes.EMA);
+  let short_value = _.last(result_short);
+  let prev_short_value = result_short[result_short.length - 2];
+  let long_value = _.last(result_long);
+  let prev_long_value = result_long[result_long.length - 2];
+
+  return shortValue < longValue && prev_short_value >= prev_long_value;
+};
+
 module.exports = {
   aboveMA,
   aboveEMA,
   belowMA,
   belowEMA,
+  bullishSMACross,
+  bearishSMACross,
+  bullishSMACross,
+  bearishSMACross,
 };
